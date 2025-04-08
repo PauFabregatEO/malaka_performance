d5470e74b1ca654abe918619a965bd1a578bbc31
migmabe <miguelmaidebezares@gmail.com>
2025-04-08 18:57:09 +0200

    New amdalh runs

---
+++ source_MMB/Amdahl_simulations/simulate3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#BSUB -J timing
+#BSUB -q hpc
+#BSUB -W 3:00
+#BSUB -n 20
+#BSUB -R "rusage[mem=512MB]"
+#BSUB -R "span[hosts=1]"
+#BSUB -R "select[model == XeonGold6226R]"
+#BSUB -o output/chunk3_%J.out
+#BSUB -e output/chunk3_%J.err
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+python -u "simulate3.py" 100
